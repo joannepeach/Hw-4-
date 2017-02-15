91b669fec32ffc897c1de2abc75a9ca11e4754de
joannepeach <jlee20@cca.edu>
2017-02-15 09:27:15 -0800

    Update sketch-5.js

--- sketch-5.js
+++ sketch-5.js
@@ -1,22 +1,15 @@
 function setup() { 
   createCanvas(400, 400); 
 }
-  colorMode(HSB);
-	
-  noStroke();
+
+	function draw() {
   fill(255);  
   background(0);
 	
-	for (var y = 20; y <= 400; y = y + 20) {
-		var startX = y;
-		var endX = 400-startX; 
-		if (y <= 200) {
-   for (var x = startX; x <= endX; x = x + 20) { 
+	for (var y = 20; y <= 200; y = y += 20) {
+   for (var x = y; x <= 400-y; x = x += 20) { 
   ellipse (x, y, 18, 18);
    }	
-	} else {
-		for (var x = endX; x <= startX; x = x + 20) {
-		ellipse(x, y, 18, 18);
 	}
-}
 	}
+	
